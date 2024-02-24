78535b08473c02bb13aeef4b718d157667ee35ad
sevalenciagr <sevalenciagr@gmail.com>
2024-02-23 19:07:34 -0500

    Consumo de datos en tabla

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'countries-by-capital-page',
@@ -7,7 +9,18 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+  public countries: Country[] = [];
+
+  constructor(
+    private countriesService: CountriesService
+  ) {}
+
   searchByCapital( term: string ): void {
+    this.countriesService.searchCapital( term )
+      .subscribe( countries => {
+        this.countries = countries;
+      })
+
     console.log('Desde ByCapitalPage');
     console.log({ term });
 
